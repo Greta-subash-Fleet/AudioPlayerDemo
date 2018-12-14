00dadd78774e08dd468af7375ed8828658c17056
Subash Parajuli <subash.parajuli@newitventure.com>
2018-12-14 16:31:42 +0545

    Initial Commit

---
+++ AudioPlayerDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AudioPlayerDemo
+//
+//  Created by Subash Parajuli on 12/14/18.
+//  Copyright © 2018 NITV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
